11922596a853d76874fccd47f47c42ab11fb200c
ryanj <ryan.jarvinen@gmail.com>
2015-06-08 19:11:14 -0700

    support nonstandard websockets

--- static/js/config.js
+++ static/js/config.js
@@ -9,10 +9,14 @@ d3demo.config = (function() {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
   }
-
+  var ws_port = ''
+  if (window.location.host.indexOf('rhcloud.com') > 0 ||
+      window.location.host.indexOf('jbosskeynote.com') > 0 ){
+    ws_port = ':8000'
+  }
   return {
       backend: {
-        ws: 'ws://' + window.location.host
+        ws: 'ws://' + window.location.host + ws_port
       }
     , playback: {
         rate: getParameterByName('rate') || 600
